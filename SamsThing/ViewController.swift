bdf63b7486c60ae263166dcbcdc5059dc281e282
Josh Edson <josh@alphamonkey.us>
2017-12-19 17:29:13 -0800

    Initial Commit

---
+++ SamsThing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SamsThing
+//
+//  Created by Josh Edson on 12/19/17.
+//  Copyright © 2017 Josh Edson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
